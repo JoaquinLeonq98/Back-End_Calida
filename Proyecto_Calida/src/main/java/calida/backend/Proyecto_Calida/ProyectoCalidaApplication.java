13d507f7c0d357b852dd2443ca9b21867c20da28
crismoute <cristianmotaramirez92@gmail.com>
2022-03-30 16:22:58 -0600

    Se creo proyecto SpringBoot

---
+++ Proyecto_Calida/src/main/java/calida/backend/Proyecto_Calida/ProyectoCalidaApplication.java
@@ -0,0 +1,13 @@
+package calida.backend.Proyecto_Calida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCalidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCalidaApplication.class, args);
+	}
+
+}
