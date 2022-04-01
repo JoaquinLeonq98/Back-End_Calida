0451d571655f41b43089f44a5e79b2dbf316720c
lbca <calberto.lopezbenitez@gmail.com>
2022-03-31 20:11:53 -0600

    Se agrga API para la DB y corrige version del build.gradle

---
+++ projectEcommerce/src/main/java/calida/projectEcommerce/ProjectEcommerceApplication.java
@@ -0,0 +1,13 @@
+package calida.projectEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEcommerceApplication.class, args);
+	}
+
+}
