1243f5d236cc65b5e381f0da275e0ba1bec6c874
lbca <calberto.lopezbenitez@gmail.com>
2022-04-01 00:31:56 -0600

    Se agrega componentes a Productos: Controller y Service

--- projectEcommerce/src/main/java/calida/projectEcommerce/controller/ProductosController.java
+++ projectEcommerce/src/main/java/calida/projectEcommerce/controller/ProductosController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import calida.projectEcommerce.model.Producto;
 import calida.projectEcommerce.service.ProductoService;
@@ -40,5 +42,15 @@ public class ProductosController {
 	public Producto addProducto(@RequestBody Producto producto) {
 	return productoService.addProducto(producto);
 	}//addProducto
+	
+	@PutMapping(path="{prodId}")
+	public Producto updateProducto(@PathVariable("prodId") Long prodId, 
+		@RequestParam (required=false) String nombre, 
+		@RequestParam (required=false) String descripcion, 
+		@RequestParam (required=false) String URL_imagen, 
+		@RequestParam (required=false) double precio,
+		@RequestParam (required=false) int inventario) {
+		return productoService.updateProducto(prodId,nombre,descripcion,URL_imagen, precio);
+	}//updateProducto
 	
 }//class ProductosController
