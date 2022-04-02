0d69c6380c42ba20d4dd66c5496531a6909e1fba
lbca <calberto.lopezbenitez@gmail.com>
2022-04-02 01:52:55 -0600

    Se agrega Model, Controller y Service de Mensaje, falta metodo post

---
+++ projectEcommerce/src/main/java/calida/projectEcommerce/service/MensajesRepository.java
@@ -0,0 +1,13 @@
+package calida.projectEcommerce.service;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import calida.projectEcommerce.model.Mensaje;
+import calida.projectEcommerce.model.Producto;
+
+public interface MensajesRepository extends JpaRepository<Mensaje, Long>{	
+	@Query("SELECT message FROM Mensaje message WHERE message.correo=?1")	 //JPQL usado para consultas
+	Optional<Producto> findByCorreo(String correo);
+	//Si optional ecuentra el producto lo regresa. Si no, no pasa nada es opcional
+
+}//interface MensajesRepository 
